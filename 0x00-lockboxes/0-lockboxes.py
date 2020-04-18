a73b7c2e6c6c601aa2c9a9e4cc81dfba80355b5d
jorgezafra94 <947@holbertonschool.com>
2020-04-18 18:57:21 +0000

    [update] list of list boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -37,8 +37,11 @@ def canUnlockAll(boxes):
     Return: - true: if all boxes inside boxes could be opened
             - false: if not all could be opened
     """
-    if (len(boxes) == 0):
-        return True
+    if (len(boxes) == 0 or type(boxes) is not list):
+        return False
+    for elem in boxes:
+        if type(elem) is not list:
+            return False
     result = recursion(boxes, [0], boxes[0])
     if(len(result) == len(boxes)):
         return True
