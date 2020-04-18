b0d2ffab8b9c16cc0da7c9aac7f5acf5a7e8e7df
jorgezafra94 <947@holbertonschool.com>
2020-04-18 20:23:57 +0000

    [feature] change recursion by iterative form

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,29 +4,7 @@ You have n number of locked boxes in front of you. Each box is numbered
 sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 
 the idea is to create a program to say if all the boxes can be opened or not
-"""
-
-
-def recursion(boxes, openBox, current):
-    """
-    recursion: function that go through all the posible keys in order
-               to return an array with all the opened boxes.
-               the idea is to save the different keys to get only the ones that
-               we need and not repeated ones
-    @boxes: list of boxes to open
-    @openBox: list with opened boxes
-    @current: actual box
-    Return: an array with boxes that could be opened
-    """
-    if (len(current) > 0):
-        for elem in current:
-            if (elem < len(boxes) and elem not in openBox):
-                openBox.append(elem)
-                r = recursion(boxes, openBox, boxes[elem])
-                openBox = r
-        return openBox
-    else:
-        return openBox
+ """
 
 
 def canUnlockAll(boxes):
@@ -42,7 +20,24 @@ def canUnlockAll(boxes):
     for elem in boxes:
         if type(elem) is not list:
             return False
-    result = recursion(boxes, [0], boxes[0])
-    if(len(result) == len(boxes)):
+
+    openBox = [0]
+
+    while (1):
+        cajas = []
+        for elem in openBox:
+            for keys in boxes[elem]:
+                if (keys < len(boxes)):
+                    cajas.append(keys)
+        cajas = list(set(cajas))
+        flag = 0
+        for elem in cajas:
+            if (elem not in openBox):
+                openBox.append(elem)
+                flag = 1
+        if (flag == 0):
+            break
+
+    if (len(openBox) == len(boxes)):
         return True
     return False
