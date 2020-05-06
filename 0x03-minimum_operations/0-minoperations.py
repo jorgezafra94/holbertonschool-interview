df6c8866d75c53352c3e4a4f36661081549f8735
jorgezafra94 <947@holbertonschool.com>
2020-05-06 22:00:48 +0000

    [feature] minoperations Algorithm

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+minimun number of operations
+"""
+
+
+def minOperations(n):
+    """
+    n = Final number of H
+    """
+    Actual = 'H'
+    operations = 0
+
+    if type(n) is not int or n < 1:
+        return 0
+
+    while (n != len(Actual)):
+
+        if operations == 0:
+            CopyAll = Actual
+            Paste = Actual + CopyAll
+            operations += 2
+
+        else:
+            if (n % len(Actual) != 0):
+                Paste = Actual + CopyAll
+                operations += 1
+
+            else:
+                CopyAll = Actual
+                Paste = Actual + CopyAll
+                operations += 2
+
+        Actual = Paste
+
+    print(Actual)
+    return operations
