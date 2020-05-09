6a5a8a117a6b343a26ec81fded455eff755770c9
jorgezafra94 <947@holbertonschool.com>
2020-05-09 21:43:06 +0000

    [update] change string logic to numbers logic

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -8,25 +8,23 @@ def minOperations(n):
     """
     n = Final number of H
     """
-    Actual = 'H'
-    CopyAll = Actual
+    Actual = 1
     operations = 0
 
     if type(n) is not int or n < 2:
         return 0
 
-    else:
-        while (len(Actual) != n):
+    while (Actual != n):
 
-            if (n % len(Actual) == 0):
-                CopyAll = Actual
-                Paste = Actual + CopyAll
-                operations += 2
+        if (n % Actual == 0):
+            CopyAll = Actual
+            Paste = Actual + CopyAll
+            operations += 2
 
-            else:
-                Paste = Actual + CopyAll
-                operations += 1
+        else:
+            Paste = Actual + CopyAll
+            operations += 1
 
-            Actual = Paste
+        Actual = Paste
 
-        return operations
+    return operations
