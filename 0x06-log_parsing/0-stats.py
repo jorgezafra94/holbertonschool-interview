b1231fae4c43d3e5d67857f742c45b0be86d5213
jorgezafra94 <947@holbertonschool.com>
2020-05-31 18:31:39 +0000

    [up] adding conditional of == 7

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -10,24 +10,26 @@ status_list = ["200", "301", "400", "401", "403", "404", "405", "500"]
 value_list = [0, 0, 0, 0, 0, 0, 0, 0]
 total_size = 0
 counter = 0
+
 try:
     for line in sys.stdin:
-        if counter < 10:
-            line_parts = shlex.split(line)
-            status = line_parts[5]
-            size = line_parts[6]
+        if len(shlex.split(line)) == 7:
+            if counter < 10:
+                line_parts = shlex.split(line)
+                status = line_parts[5]
+                size = line_parts[6]
 
-            total_size += int(size)
-            for i in range(len(status_list)):
-                if status_list[i] == status:
-                    value_list[i] += 1
-            counter += 1
-        else:
-            print("File size: {}".format(total_size))
-            for i in range(len(status_list)):
-                if value_list[i] > 0:
-                    print("{}: {}".format(status_list[i], value_list[i]))
-            counter = 0
+                total_size += int(size)
+                for i in range(len(status_list)):
+                    if status_list[i] == status:
+                        value_list[i] += 1
+                    counter += 1
+            else:
+                print("File size: {}".format(total_size))
+                for i in range(len(status_list)):
+                    if value_list[i] > 0:
+                        print("{}: {}".format(status_list[i], value_list[i]))
+                counter = 0
 
 except Exception:
     pass
