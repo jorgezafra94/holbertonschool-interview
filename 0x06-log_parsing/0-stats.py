55374a705502ae81e560a2ea0730612598555e18
jorgezafra94 <947@holbertonschool.com>
2020-05-31 18:44:41 +0000

    [fix] change conditional

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -13,26 +13,26 @@ counter = 0
 
 try:
     for line in sys.stdin:
-
         line_parts = shlex.split(line)
         if len(line_parts) > 2:
-            status = line_parts[-2]
-            size = line_parts[-1]
+            if counter < 10:
+                status = line_parts[-2]
+                size = line_parts[-1]
 
-            total_size += int(size)
-            for i in range(len(status_list)):
-                if status_list[i] == status:
-                    value_list[i] += 1
-            counter += 1
+                total_size += int(size)
+                for i in range(len(status_list)):
+                    if status_list[i] == status:
+                        value_list[i] += 1
+                counter += 1
 
-            if counter >= 10:
+            else:
                 print("File size: {}".format(total_size))
                 for i in range(len(status_list)):
                     if value_list[i] > 0:
                         print("{}: {}".format(status_list[i], value_list[i]))
                 counter = 0
 
-except Exception:
+except:
     pass
 
 finally:
