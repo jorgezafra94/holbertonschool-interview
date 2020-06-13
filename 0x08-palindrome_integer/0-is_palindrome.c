da686dc5c25a25f1dbe850712c20e4e0cb4af2fb
jorgezafra94 <947@holbertonschool.com>
2020-06-13 18:18:39 +0000

    [fe] function that determines if a number is palindrome or not

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - function that says if a number is palidrome or not
+ * @n: number to check
+ * Return: 1 if it is palindrome or 0 if not
+ */
+int is_palindrome(unsigned long n)
+{
+	unsigned long digits = 1;
+	int first, last, counter = 0, aux = 0;
+
+	/* we get the max number of digits */
+	while ((n % digits) != n)
+	{
+		digits = digits * 10;
+		counter++;
+	}
+
+	digits = digits / 10;
+
+	/* now we start the comparison and then we remove that numbers*/
+	while (counter > aux)
+	{
+		first = n / digits;
+		last = n % 10;
+
+		if (first != last)
+			return (0);
+
+		/* remove the first and last */
+		n = n % digits;
+		n = n / 10;
+
+		digits = digits / 100;
+		counter--;
+		aux++;
+	}
+
+	return (1);
+
+}
