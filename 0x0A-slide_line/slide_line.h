8a25585e89387d8c176d0c4146cb6a4368aba07c
jorgezafra94 <947@holbertonschool.com>
2020-06-29 00:50:42 +0000

    header file

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,12 @@
+#ifndef ZETA
+#define ZETA
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SLIDE_LEFT  3
+#define SLIDE_RIGHT 5
+
+
+int slide_line(int *line, size_t size, int direction);
+#endif
