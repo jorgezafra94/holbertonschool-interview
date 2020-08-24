cac23e70c93f2544f98d3046d7f29a245ac54586
jorgezafra94 <947@holbertonschool.com>
2020-08-24 00:16:47 +0000

    [fe] water contained

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -10,8 +10,23 @@ def rain(walls):
     """
     if len(walls) < 3:
         return 0
-    total = 0
-    for i in range(1, len(walls) - 1):
-        if walls[i - 1] == 0 and walls[i + 1] == 0:
-                total = total + walls[i]
-    return total
+
+    left, right = ([], [])
+    for i in range(len(walls)):
+        left.append(0)
+        right.append(0)
+
+    left[0] = walls[0]
+    for i in range(1, len(walls)):
+        left[i] = max(left[i-1], walls[i])
+
+    right[-1] = walls[-1]
+    for i in range(len(walls) - 2, -1, -1):
+        right[i] = max(right[i + 1], walls[i])
+
+    rain = 0
+    for i in range(0, len(walls)):
+        first = left[i]
+        second = right[i]
+        rain += min(first, second) - walls[i]
+    return rain
