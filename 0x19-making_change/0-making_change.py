f3ef09cd14ff3432461cdf0591e920f1f4029cab
jorgezafra94 <jorgezafra94@hotmail.com>
2020-11-09 07:24:29 -0500

    [fe] change recursion by while loops

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -4,44 +4,29 @@ number of coins to get a number
 """
 
 
-def recursive(coins, total, pos, final, count):
-    """
-    this is the recursive function to know how many
-    coins you will need to complete the total
-    """
-    if total == 0:
-        return count
-
-    if total >= coins[pos]:
-        new_total = total - coins[pos]
-        result = recursive(coins, new_total, pos, final, count + 1)
-        return result
-
-    else:
-        if (pos + 1) < final:
-            result = recursive(coins, total, pos + 1, final, count)
-            return result
-        else:
-            return -1
-
-
 def makeChange(coins, total):
     """
-    Return: fewest number of coins needed to meet total
-    * If total is 0 or less, return 0
-    * f total cannot be met by any number of coins you have
-      return -1
+    determining how many coins do i have to use to
+    get the total amount
     """
+    sum = 0
+    i = 0
+    counter = 0
 
     if total <= 0:
         return 0
 
-    sorted_coins = sorted(coins, reverse=True)
-    aux = len(sorted_coins)
+    new_coins = sorted(coins, reverse=True)
+    my_coins = len(new_coins)
+    aux = total - sum
 
-    for pos in range(aux):
-        count = 0
-        result = recursive(sorted_coins, total, pos, aux, count)
-        if result > 0:
-            return result
+    while sum < total and i < my_coins:
+        while new_coins[i] <= aux:
+            sum = sum + new_coins[i]
+            counter += 1
+            if sum == total:
+                return counter
+            aux = total - sum
+        i += 1
+
     return -1
