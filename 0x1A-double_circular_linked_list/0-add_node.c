fe0680b58f4c0feb5f260b7f959f5127e267fe08
jorgezafra94 <jorgezafra94@hotmail.com>
2020-11-14 11:17:09 -0500

    [fe] logic of end and beginning

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,103 @@
+#include "list.h"
+/**
+ * first - add the first node
+ * @list: circular linked list
+ * @str: string to insert
+ * Return: new node, or NULL on failure
+ */
+List *first(List **list, char *str)
+{
+	List *new;
+
+	new = malloc(sizeof(List));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+
+	new->next = new;
+	new->prev = new;
+	*list = new;
+
+	return (new);
+}
+
+/**
+ * add_node_begin - adds node at the beginning
+ * @list: circular linked list
+ * @str: string to insert
+ * Return: new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *new, *temp, *aux;
+
+	if (list == NULL)
+		return (NULL);
+
+	if (*list == NULL)
+		return (first(list, str));
+
+	aux = *list;
+	new = malloc(sizeof(List));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->next = aux;
+	new->prev = aux->prev;
+	temp = aux->prev;
+	aux->prev = new;
+	temp->next = new;
+	*list = new;
+
+	return (new);
+}
+
+/**
+ * add_node_end - adds node at the end
+ * @list: circular linked list
+ * @str: string to insert
+ * Return: new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *new, *tmp, *aux;
+
+	if (list == NULL)
+		return (NULL);
+
+	if (*list == NULL)
+		return (first_node(list, str));
+
+	aux = *list;
+	new = malloc(sizeof(List));
+    
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->next = aux;
+	new->prev = aux->prev;
+	tmp = aux->prev;
+	aux->prev = new;
+	tmp->next = new;
+
+	return (new);
+}
